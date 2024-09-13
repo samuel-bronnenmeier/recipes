3ee5eab45f7756ea4eb16b434abaf97913aca826
samuel-bronnenmeier <samuelbronnenmeier@gmail.com>
2024-09-13 20:55:01 +0200

    Design table creation commands

--- recipes.db.sql
+++ recipes.db.sql
@@ -1,3 +1,26 @@
-CREATE TABLE main_dishes (
-    
+CREATE TABLE Recipes (
+    id INT PRIMARY KEY,
+    category varchar(255),
+    recipe_name varchar(511),
 );
+
+CREATE TABLE Ingredients (
+    id INT PRIMARY KEY,
+    ingredient_name varchar(255),
+);
+
+CREATE TABLE rec_ing (
+    rec_id INT,
+    ing_id INT,
+    amount INT,
+    measerument varchar(255),
+    extra varchar(511)
+);
+
+CREATE TABLE Instructions (
+    step INT,
+    instruction TEXT,
+    rec_id INT
+)
+
+/* TODO: Add primary keys */
