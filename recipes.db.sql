e2b6a9bf2346e863ef203c463d56e8da002131f2
samuel-bronnenmeier <samuelbronnenmeier@gmail.com>
2024-09-13 21:05:30 +0200

    Create tables

--- recipes.db.sql
+++ recipes.db.sql
@@ -1,12 +1,12 @@
 CREATE TABLE Recipes (
     id INT PRIMARY KEY,
     category varchar(255),
-    recipe_name varchar(511),
+    recipe_name varchar(511)
 );
 
 CREATE TABLE Ingredients (
     id INT PRIMARY KEY,
-    ingredient_name varchar(255),
+    ingredient_name varchar(255)
 );
 
 CREATE TABLE rec_ing (
@@ -14,13 +14,15 @@ CREATE TABLE rec_ing (
     ing_id INT,
     amount INT,
     measerument varchar(255),
-    extra varchar(511)
+    extra varchar(511),
+    FOREIGN KEY (rec_id) REFERENCES Recipes(id),
+    FOREIGN KEY (ing_id) REFERENCES Ingredients(id)
 );
 
 CREATE TABLE Instructions (
     step INT,
     instruction TEXT,
-    rec_id INT
+    rec_id INT,
+    PRIMARY KEY (step, rec_id),
+    FOREIGN KEY (rec_id) REFERENCES Recipes(id)
 )
-
-/* TODO: Add primary keys */
